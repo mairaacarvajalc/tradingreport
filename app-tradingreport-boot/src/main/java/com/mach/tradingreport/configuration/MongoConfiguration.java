4c4ba500444c2ab527d9568681d38e89a4315b34
Alejandra Carvajal <mairaa.carvajalc@ecci.edu.co>
2022-10-18 18:48:03 +0200

    * Create configuration

--- app-tradingreport-boot/src/main/java/com/mach/tradingreport/configuration/MongoConfiguration.java
+++ app-tradingreport-boot/src/main/java/com/mach/tradingreport/configuration/MongoConfiguration.java
@@ -1,4 +1,27 @@
 package com.mach.tradingreport.configuration;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
 public class MongoConfiguration {
+
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public MongoClient mongoClient(@Value("${spring.mongodb.uri}") String connectionUrl){
+        ConnectionString connString = new ConnectionString(connectionUrl);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
 }
